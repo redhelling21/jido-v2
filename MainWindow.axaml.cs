9f31f6730e24fa3e269618295e399a873cde21b2
BURI Théo <theo.buri@gmail.com>
2024-02-04 23:30:15 +0100

    First skeleton

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace jido_v2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
