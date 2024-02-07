97ea7c5c14ba82d4af8349d5a3180de69a95007e
Théo Buri <redhelling21@users.noreply.github.com>
2024-02-07 09:14:00 +0100

    Added basic MVVM skeleton (#3)
    * First skeleton

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jido-v2
 Jido mais en C#, et en mieux (j'espère)
+
+## Useful links
+https://github.com/AvaloniaUI/Avalonia.Samples/tree/main/src/Avalonia.Samples/Routing/BasicViewLocatorSample
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace jido_v2.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
