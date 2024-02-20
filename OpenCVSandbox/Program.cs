61c7f67aca421e681fef937f9038f456030f28d3
redhelling21 <theo.buri.external-provider@kuhn.com>
2024-02-20 18:13:45 +0100

    Added autoloot ui (unplugged)

--- OpenCVSandbox/Program.cs
+++ OpenCVSandbox/Program.cs
@@ -1,9 +1,5 @@
 // See https://aka.ms/new-console-template for more information
-using System.Drawing.Imaging;
 using OpenCVSandbox.Tests;
-using OpenCvSharp;
-using OpenCvSharp.Extensions;
-using Point = OpenCvSharp.Point;
 
 internal class Program
 {
