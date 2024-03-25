49cf0124a2707960808e855174b137dd15c053c4
redhelling21 <theo.buri.external-provider@kuhn.com>
2024-03-25 16:52:15 +0100

    temp

---
+++ Models/Commands.cs
@@ -0,0 +1,49 @@
+using SharpHook.Native;
+using System;
+using System.Collections.Generic;
+
+namespace Jido.Models
+{
+    public abstract class LowLevelCommand
+    {
+    }
+
+    public class HighLevelCommand
+    {
+        public int IntervalInMs { get; set; }
+        public bool IsComposite => CompositeCommand != null;
+        public LowLevelCommand? Command { get; set; }
+        public CompositeCommand? CompositeCommand { get; set; }
+
+        public HighLevelCommand(LowLevelCommand command, int interval)
+        {
+            if (interval == 0)
+            {
+                throw new ArgumentException("Interval cannot be 0");
+            }
+            Command = command;
+            IntervalInMs = interval;
+        }
+    }
+
+    public class ConstantCommand
+    {
+        public KeyCode KeyToPress { get; set; }
+    }
+
+    public class PressCommand : LowLevelCommand
+    {
+        public KeyCode KeyToPress { get; set; }
+        public int PressDurationInMs { get; set; } = 100;
+    }
+
+    public class WaitCommand : LowLevelCommand
+    {
+        public int WaitTimeInMs { get; set; }
+    }
+
+    public class CompositeCommand
+    {
+        public List<LowLevelCommand> Commands { get; set; }
+    }
+}
--- Services/AutopressService.cs
+++ Services/AutopressService.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
 using Jido.Config;
+using Jido.Models;
 using Jido.Utils;
 using OpenCvSharp;
 using SharpHook;
@@ -16,19 +18,17 @@ namespace Jido.Services
     public class AutopressService : IAutopressService
     {
         private IKeyHooksManager _keyHooksManager;
+        private EventSimulator _eventSimulator = new EventSimulator();
         private CancellationTokenSource _cancellationTokenSource;
         private JidoConfig _config;
         private KeyCode _toggleKey;
-        private List<Color> _colors;
+        private List<HighLevelCommand> _commands;
+        private List<ConstantCommand> _constantCommands;
+        private ConcurrentQueue<LowLevelCommand> _queuedCommands;
 
         public KeyCode ToggleKey
         {
             get => _toggleKey;
-        }
-
-        public List<Color> Colors
-        {
-            get => _colors;
         }
 
         public ServiceStatus Status { get; set; } = ServiceStatus.STOPPED;
@@ -67,7 +67,7 @@ namespace Jido.Services
             {
                 Status = ServiceStatus.IDLE;
                 _cancellationTokenSource = new CancellationTokenSource();
-                Task.Run(() => AutopressRoutine(_cancellationTokenSource.Token));
+                Task.Run(() => MainRoutine(_cancellationTokenSource.Token));
             }
             else
             {
@@ -77,8 +77,52 @@ namespace Jido.Services
             StatusChanged?.Invoke(this, Status);
         }
 
-        private async Task AutopressRoutine(CancellationToken cancellationToken)
-        { }
+        private async Task MainRoutine(CancellationToken cancellationToken)
+        {
+            // Setup
+            foreach (var command in _constantCommands)
+            {
+                _eventSimulator.SimulateKeyPress(command.KeyToPress);
+            }
+
+            // Loops
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(100);
+            }
+
+            // Teardown
+            foreach (var command in _constantCommands)
+            {
+                _eventSimulator.SimulateKeyRelease(command.KeyToPress);
+            }
+        }
+
+        private async Task KeyPressRoutine(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var foundWork = _queuedCommands.TryDequeue(out var command);
+                if (foundWork)
+                {
+                    if (command is WaitCommand waitCommand)
+                    {
+                        await Task.Delay(waitCommand.WaitTimeInMs);
+                    }
+                    else if (command is PressCommand pressCommand)
+                    {
+                        await Task.Delay(300);
+                        _eventSimulator.SimulateKeyPress(pressCommand.KeyToPress);
+                        await Task.Delay(pressCommand.PressDurationInMs);
+                        _eventSimulator.SimulateKeyRelease(pressCommand.KeyToPress);
+                    }
+                }
+                else
+                {
+                    await Task.Delay(100);
+                }
+            }
+        }
     }
 
     public interface IAutopressService : IServiceWithStatus
